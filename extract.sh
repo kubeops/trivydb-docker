fcdae2321dafea9847da07637812a850986a3b7b
Tamal Saha <tamal@appscode.com>
2023-03-26 10:53:15 -0700

    Update to trivy v0.38 (#3)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- extract.sh
+++ extract.sh
@@ -4,17 +4,25 @@
 set -eou pipefail
 
 FILESERVER_ADDR=${FILESERVER_ADDR:-https://scanner}
+TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 
 mkdir -p trivy/db
 cd trivy/db
-
-export TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 curl -fSsLO -k \
     -H "Authorization: Bearer ${TOKEN}" \
     ${FILESERVER_ADDR}/files/trivy/db.tar.gz
-
 tar xvf db.tar.gz
 rm db.tar.gz
+
+cd ../..
+
+mkdir -p trivy/java-db
+cd trivy/java-db
+curl -fSsLO -k \
+    -H "Authorization: Bearer ${TOKEN}" \
+    ${FILESERVER_ADDR}/files/trivy/javadb.tar.gz
+tar xvf javadb.tar.gz
+rm javadb.tar.gz
 
 /root/.cache/tv version
 /bin/oras version
