a82e38341676c838d6232d8e79f8dc4cb50e1807
Tamal Saha <tamal@appscode.com>
2024-06-22 01:00:53 -0700

    Make db registry configurable
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- update-trivydb.sh
+++ update-trivydb.sh
@@ -4,10 +4,11 @@
 set -eou pipefail
 
 FILESERVER_ADDR=${FILESERVER_ADDR:-https://scanner}
+REGISTRY=${REGISTRY:-ghcr.io}
 
 # https://aquasecurity.github.io/trivy/v0.38/docs/advanced/air-gap/
 # busybox is missing ca-certificates
-oras pull ghcr.io/aquasecurity/trivy-db:2 --insecure
+oras pull ${REGISTRY}/aquasecurity/trivy-db:2 --insecure
 tar xvf db.tar.gz
 
 # oras pulls in tar format. File will be named `db.tar.gz`
@@ -21,7 +22,7 @@ curl -fSsL -X POST -F 'file=@metadata.json' \
     --cacert /var/serving-cert/ca.crt \
     ${FILESERVER_ADDR}/files/trivy/
 
-oras pull ghcr.io/aquasecurity/trivy-java-db:1 --insecure
+oras pull ${REGISTRY}/aquasecurity/trivy-java-db:1 --insecure
 curl -fSsL -X POST -F 'file=@javadb.tar.gz' \
     -H "Authorization: Bearer ${TOKEN}" \
     --cacert /var/serving-cert/ca.crt \
